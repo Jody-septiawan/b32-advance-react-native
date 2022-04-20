bb36bd6275553a65a2b5ae718d54bda29f0ff751
Jody-septiawan <jody.septiawan5@gmail.com>
2022-04-20 09:56:47 +0700

    update

--- src/screens/hello.js
+++ src/screens/hello.js
@@ -1,15 +1,23 @@
-import * as React from "react";
-import { Text, Box } from "native-base";
+import * as React from 'react';
+import { Text, Box, Pressable } from 'native-base';
 
 // Add Props in Hello({navigation})
-export default function Hello() {
+export default function Hello({ navigation }) {
   return (
     <Box bg="primary.400" flex={1} alignItems="center" justifyContent="center">
       <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30}>
         Life's too short
       </Text>
 
-      {/* Create Button */}
+      <Pressable
+        onPress={() => navigation.navigate('IncDec')}
+        mt={10}
+        bg="primary.800"
+        p={2}
+        borderRadius={10}
+      >
+        <Text>Move to Screen IncDec</Text>
+      </Pressable>
     </Box>
   );
 }
--- src/screens/incDec.js
+++ src/screens/incDec.js
@@ -1,10 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { TouchableOpacity } from "react-native";
-import { Text, Box } from "native-base";
-import { backgroundColor, height } from "styled-system";
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Text, Box } from 'native-base';
+import { backgroundColor, height } from 'styled-system';
 
-export default function incDec() {
+export default function IncDec() {
   // Init State
   const [counter, setCounter] = useState(0);
 
@@ -34,36 +34,36 @@ export default function incDec() {
         click the less button it will decrease by one
       </Text>
 
-      <Text fontSize={50} style={{ color: "#273c75" }}>
+      <Text fontSize={50} style={{ color: '#273c75' }}>
         {counter}
       </Text>
 
       <TouchableOpacity
         onPress={Add}
         style={{
-          backgroundColor: "#487eb0",
+          backgroundColor: '#487eb0',
           height: 40,
-          width: "100%",
-          alignItems: "center",
-          justifyContent: "center",
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
           borderRadius: 10,
           margin: 10,
         }}
       >
-        <Text color={{ color: "white" }}>Add</Text>
+        <Text color={{ color: 'white' }}>Add</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={Less}
         style={{
-          backgroundColor: "#487eb0",
+          backgroundColor: '#487eb0',
           height: 40,
-          width: "100%",
-          alignItems: "center",
-          justifyContent: "center",
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
           borderRadius: 10,
         }}
       >
-        <Text color={{ color: "white" }}>Less</Text>
+        <Text color={{ color: 'white' }}>Less</Text>
       </TouchableOpacity>
     </Box>
   );
