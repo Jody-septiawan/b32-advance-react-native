257eab6c5b30cfb8c534c6d5a790a6830e12ca03
Jody-septiawan <jody.septiawan5@gmail.com>
2022-04-20 10:49:30 +0700

    update

--- src/screens/detailSoc.js
+++ src/screens/detailSoc.js
@@ -1,8 +1,9 @@
-import * as React from "react";
-import { Text, Box } from "native-base";
+import * as React from 'react';
+import { Text, Box } from 'native-base';
 
 // Declare route
-export default function DetailSoc({}) {
+export default function DetailSoc({ route }) {
+  console.log(route);
   return (
     <Box
       safeArea
@@ -13,7 +14,7 @@ export default function DetailSoc({}) {
       p={10}
     >
       <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30}>
-        {/* Implement Params to get Data on previous screen  */}
+        {route.params.value}
       </Text>
     </Box>
   );
