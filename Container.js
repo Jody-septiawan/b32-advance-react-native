fa12fe690cd71b0b254ad58e09743541d55702a7
Jody-septiawan <jody.septiawan5@gmail.com>
2022-04-20 10:25:39 +0700

    update

--- Container.js
+++ Container.js
@@ -1,29 +1,61 @@
-import * as React from "react";
+import * as React from 'react';
 
 //Import Navigation Container
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer } from '@react-navigation/native';
 
 // Import Stack Navigation
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator } from '@react-navigation/stack';
 
 //Import Bottom Tab Navigation
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 //Import Icon
+import { Ionicons } from '@expo/vector-icons';
 
 // Import Theme Native Base
-import { useTheme } from "native-base";
+import { useTheme } from 'native-base';
 
 // Import Screen
-import FormNativeBase from "./src/screens/formNativeBase";
-import Hello from "./src/screens/hello";
-import IncDec from "./src/screens/incDec";
+import FormNativeBase from './src/screens/formNativeBase';
+import Hello from './src/screens/hello';
+import IncDec from './src/screens/incDec';
 
 // Create Stack Navigation
 const Stack = createStackNavigator();
 
 //Create Bottom Tab Navigation
+const Tab = createBottomTabNavigator();
 
 // Create Component Bottom Tab Navigation
+function MyTab() {
+  const theme = useTheme();
+
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name == 'Home') {
+            iconName = focused ? 'ios-home' : 'ios-home-outline';
+          } else if (route.name == 'Form') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: theme.colors.primary['800'],
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Home" component={Hello} />
+      <Tab.Screen name="Form" component={FormNativeBase} />
+    </Tab.Navigator>
+  );
+}
 
 export default function Container() {
   // Init Theme
@@ -31,28 +63,9 @@ export default function Container() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerMode: "screen",
-          headerTintColor: "white",
-          headerStyle: { backgroundColor: theme.colors.primary["300"] },
-        }}
-      >
-        <Stack.Screen
-          name="Home"
-          component={Hello}
-          options={{
-            title: "Hello Screen",
-          }}
-        />
-        <Stack.Screen
-          name="IncDec"
-          component={IncDec}
-          options={{
-            title: "Increment Decrement",
-          }}
-        />
+      <Stack.Navigator initialRouteName="Home">
+        {/* <Stack.Screen name="Main" component={MyTab} /> */}
+        <Stack.Screen name="Home" component={Hello} />
       </Stack.Navigator>
     </NavigationContainer>
   );
